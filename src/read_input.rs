f3a71a49d170189d1c80182636a4806461ced330
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-12-10 00:24:02 +0800

    add license

--- src/read_input.rs
+++ src/read_input.rs
@@ -1,3 +1,19 @@
+/*  A program to convert markup scripts to html for viewing.
+    Copyright (C) 2022 Sherlock Holmes
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 pub mod read_args {
     pub fn read_input_file() -> String {
         use std::env;
